703a808682623cf9a597399856183cdd361a3804
dgonzalo05 <dgonzalo@iesjoandaustria.org>
2018-06-01 18:26:37 +0200

    fet

--- Main.java
+++ Main.java
@@ -1,9 +1,7 @@
 // Dani Gonzalo
 import java.awt.EventQueue;
-import javax.xml.*;
-
+import player.AI;
 import player.Player;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -15,19 +13,15 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
 import javax.swing.SwingConstants;
-import javax.swing.UIManager;
 
 public class Main extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField txtPlayer1;
 	private JTextField txtPlayer2;
+	private JTextField txtSinglePlayer;
+	private boolean singlePlayer;
 
 	/**
 	 * Launch the application.
@@ -65,19 +59,31 @@ public class Main extends JFrame {
 		contentPane.add(label);
 		
 		JLabel lblO = new JLabel("O");
+		lblO.setVisible(false);
 		lblO.setForeground(Color.BLACK);
 		lblO.setFont(new Font("Arial Black", Font.BOLD, 20));
 		lblO.setBounds(23, 397, 33, 16);
 		contentPane.add(lblO);
 		
 		JLabel lblX = new JLabel("X");
+		lblX.setVisible(false);
 		lblX.setForeground(Color.BLACK);
 		lblX.setFont(new Font("Arial Black", Font.BOLD, 20));
 		lblX.setBounds(579, 397, 33, 16);
 		contentPane.add(lblX);
 		
+		
+		// 1 jugador 
+		txtSinglePlayer = new JTextField();
+		txtSinglePlayer.setVisible(false);
+		txtSinglePlayer.setFont(new Font("Arial", Font.PLAIN, 20));
+		txtSinglePlayer.setText("Player 1");
+		txtSinglePlayer.setBounds(200, 385, 244, 43);
+		contentPane.add(txtSinglePlayer);
+		
 		// Text Jugador 1
 		txtPlayer1 = new JTextField();
+		txtPlayer1.setVisible(false);
 		txtPlayer1.setForeground(Color.BLACK);
 		txtPlayer1.setFont(new Font("Arial", Font.PLAIN, 20));
 		txtPlayer1.setText("Player1");
@@ -86,6 +92,7 @@ public class Main extends JFrame {
 		
 		// Text Jugador 2
 		txtPlayer2 = new JTextField();
+		txtPlayer2.setVisible(false);
 		txtPlayer2.setForeground(Color.BLACK);
 		txtPlayer2.setText("Player2");
 		txtPlayer2.setFont(new Font("Arial", Font.PLAIN, 20));
@@ -104,23 +111,67 @@ public class Main extends JFrame {
 		contentPane.add(lblError);
 		
 		JButton btnStart = new JButton("START");
+		btnStart.setVisible(false);
 		btnStart.setBackground(new Color(255, 215, 0));
 		btnStart.setForeground(Color.BLACK);
 		btnStart.setFont(new Font("Impact", Font.BOLD, 50));
 		btnStart.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if ((!txtPlayer1.getText().isEmpty() && !txtPlayer2.getText().isEmpty()) && (txtPlayer1.getText().length() < 20 && txtPlayer2.getText().length() < 20)) {
-					Match match = new Match(new Player(txtPlayer1.getText(),"O"), new Player(txtPlayer2.getText(), "X"));
-					match.setVisible(true);
-					dispose();
-				} else {
-					lblError.setVisible(true);
+				if (singlePlayer) {
+					if ((!txtSinglePlayer.getText().isEmpty() && txtSinglePlayer.getText().length() < 20)) {
+						Match match = new Match(new Player(txtSinglePlayer.getText(),"O"), new AI("AI","X"));
+						match.setVisible(true);
+						dispose();
+					}else {
+						lblError.setVisible(true);
+					}
+				}else {
+					if ((!txtPlayer1.getText().isEmpty() && !txtPlayer2.getText().isEmpty()) && (txtPlayer1.getText().length() < 20 && txtPlayer2.getText().length() < 20)) {
+						Match match = new Match(new Player(txtPlayer1.getText(),"O"), new Player(txtPlayer2.getText(), "X"));
+						match.setVisible(true);
+						dispose();
+					} else {
+						lblError.setVisible(true);
+					}
 				}
 			}
 		});
-		
 		btnStart.setBounds(179, 475, 268, 91);
 		contentPane.add(btnStart);
+		
+		JButton btnSinglePlayer = new JButton("SINGLE PLAYER");
+		btnSinglePlayer.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			singlePlayer = true;
+			btnStart.setVisible(true);
+			lblO.setBounds(175, 397, 33, 16);
+			lblO.setVisible(true);
+			lblX.setVisible(false);
+			txtSinglePlayer.setVisible(true);
+			txtPlayer2.setVisible(false);
+			txtPlayer1.setVisible(false);
+			}
+		});
+		btnSinglePlayer.setFont(new Font("Arial", Font.BOLD, 20));
+		btnSinglePlayer.setBounds(99, 281, 196, 43);
+		contentPane.add(btnSinglePlayer);
+		
+		JButton btnMultiplayer = new JButton("MULTIPLAYER");
+		btnMultiplayer.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			singlePlayer = false;
+			btnStart.setVisible(true);
+			txtSinglePlayer.setVisible(false);
+			txtPlayer2.setVisible(true);
+			txtPlayer1.setVisible(true);
+			lblO.setBounds(23, 397, 33, 16);
+			lblO.setVisible(true);
+			lblX.setVisible(true);
+			}
+		});
+		btnMultiplayer.setFont(new Font("Arial", Font.BOLD, 20));
+		btnMultiplayer.setBounds(325, 281, 196, 43);
+		contentPane.add(btnMultiplayer);
 		
 		JButton btnScoreboard = new JButton("Scoreboard");
 		btnScoreboard.setForeground(Color.BLACK);
@@ -128,13 +179,7 @@ public class Main extends JFrame {
 		btnScoreboard.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Scoreboard scoreboard = null;
-				try {
-					scoreboard = new Scoreboard();
-				} catch (FileNotFoundException e1) {
-					e1.printStackTrace();
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
+				scoreboard = new Scoreboard();
 				scoreboard.setVisible(true);
 				dispose();
 			}
--- Scoreboard.java
+++ Scoreboard.java
@@ -1,30 +1,18 @@
 // Dani Gonzalo
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import openfile.OpenFile;
 import java.awt.Color;
 import javax.swing.JLabel;
 import java.awt.Font;
-import java.awt.List;
-
-import javax.swing.JScrollPane;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.awt.event.ActionEvent;
-import javax.swing.JRadioButton;
-import javax.swing.JSplitPane;
-import javax.swing.ButtonGroup;
-import javax.swing.JTable;
-import javax.swing.ListModel;
+import javax.swing.DefaultListModel;
 import javax.swing.JList;
 
 public class Scoreboard extends JFrame {
@@ -34,28 +22,13 @@ public class Scoreboard extends JFrame {
 	private JList list_vict;
 	private JList list_def;
 	private JList list_draw;
-	private final ButtonGroup buttonGroup = new ButtonGroup();
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Scoreboard frame = new Scoreboard();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
 	/**
 	 * Create the frame.
 	 * @throws IOException 
 	 */
-	public Scoreboard() throws IOException {
+	
+	public Scoreboard(){
 		setTitle("Three in a Row");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 650, 650);
@@ -83,36 +56,33 @@ public class Scoreboard extends JFrame {
 		btnBack.setBounds(34, 555, 117, 25);
 		contentPane.add(btnBack);
 		
-		JRadioButton rdbtnName = new JRadioButton("Name");
-		buttonGroup.add(rdbtnName);
-		rdbtnName.setForeground(Color.WHITE);
-		rdbtnName.setBackground(Color.BLACK);
-		rdbtnName.setBounds(121, 122, 76, 23);
-		contentPane.add(rdbtnName);
+		JLabel lblName = new JLabel("Name");
+		lblName.setForeground(Color.WHITE);
+		lblName.setBackground(Color.BLACK);
+		lblName.setBounds(121, 122, 76, 23);
+		contentPane.add(lblName);
 		
-		JRadioButton rdbtnVictory = new JRadioButton("Victory");
-		buttonGroup.add(rdbtnVictory);
-		rdbtnVictory.setForeground(Color.WHITE);
-		rdbtnVictory.setBackground(Color.BLACK);
-		rdbtnVictory.setBounds(298, 122, 76, 23);
-		contentPane.add(rdbtnVictory);
+		JLabel lblVictory = new JLabel("Victory");
+		lblVictory.setForeground(Color.WHITE);
+		lblVictory.setBackground(Color.BLACK);
+		lblVictory.setBounds(298, 122, 76, 23);
+		contentPane.add(lblVictory);
 		
-		JRadioButton rdbtnDraw = new JRadioButton("Draw");
-		buttonGroup.add(rdbtnDraw);
-		rdbtnDraw.setForeground(Color.WHITE);
-		rdbtnDraw.setBackground(Color.BLACK);
-		rdbtnDraw.setBounds(491, 122, 76, 23);
-		contentPane.add(rdbtnDraw);
+		JLabel lblDraw = new JLabel("Draw");
+		lblDraw.setForeground(Color.WHITE);
+		lblDraw.setBackground(Color.BLACK);
+		lblDraw.setBounds(491, 122, 76, 23);
+		contentPane.add(lblDraw);
 		
-		JRadioButton rdbtnDefeat = new JRadioButton("Defeat");
-		buttonGroup.add(rdbtnDefeat);
-		rdbtnDefeat.setForeground(Color.WHITE);
-		rdbtnDefeat.setBackground(Color.BLACK);
-		rdbtnDefeat.setBounds(396, 122, 76, 23);
-		contentPane.add(rdbtnDefeat);
+		JLabel Defeat = new JLabel("Defeat");
+		Defeat.setForeground(Color.WHITE);
+		Defeat.setBackground(Color.BLACK);
+		Defeat.setBounds(396, 122, 76, 23);
+		contentPane.add(Defeat);
 		
-		ArrayList<ArrayList<String>> score = read();
-		list_name = new JList((ListModel) score.get(0));
+		ArrayList<DefaultListModel<String>> score = read();
+		list_name = new JList(score.get(0));
+		list_name.setEnabled(false);
 		list_name.setSelectionBackground(Color.WHITE);
 		list_name.setSelectionForeground(Color.WHITE);
 		list_name.setForeground(Color.WHITE);
@@ -120,7 +90,8 @@ public class Scoreboard extends JFrame {
 		list_name.setBounds(94, 153, 202, 374);
 		contentPane.add(list_name);
 		
-		list_vict = new JList((ListModel) score.get(1));
+		list_vict = new JList(score.get(1));
+		list_vict.setEnabled(false);
 		list_vict.setSelectionForeground(Color.WHITE);
 		list_vict.setSelectionBackground(Color.WHITE);
 		list_vict.setForeground(Color.WHITE);
@@ -128,7 +99,8 @@ public class Scoreboard extends JFrame {
 		list_vict.setBounds(313, 153, 61, 374);
 		contentPane.add(list_vict);
 		
-		list_def = new JList((ListModel) score.get(2));
+		list_def = new JList(score.get(2));
+		list_def.setEnabled(false);
 		list_def.setSelectionForeground(Color.WHITE);
 		list_def.setSelectionBackground(Color.WHITE);
 		list_def.setForeground(Color.WHITE);
@@ -136,7 +108,8 @@ public class Scoreboard extends JFrame {
 		list_def.setBounds(406, 153, 61, 374);
 		contentPane.add(list_def);
 		
-		list_draw = new JList((ListModel) score.get(3));
+		list_draw = new JList(score.get(3));
+		list_draw.setEnabled(false);
 		list_draw.setSelectionForeground(Color.WHITE);
 		list_draw.setSelectionBackground(Color.WHITE);
 		list_draw.setForeground(Color.WHITE);
@@ -144,26 +117,25 @@ public class Scoreboard extends JFrame {
 		list_draw.setBounds(501, 153, 61, 374);
 		contentPane.add(list_draw);
 	}
-	public ArrayList<ArrayList<String>> read() throws IOException {
+	
+	public ArrayList<DefaultListModel<String>> read() {
 		BufferedReader file = null;
-		ArrayList <ArrayList<String>> values = new ArrayList<ArrayList<String>>();
+		ArrayList <DefaultListModel<String>> values = new ArrayList<DefaultListModel<String>>();
+		
 		try {
 			file = OpenFile.r("./src/score/score");
 			String line;
-			values.add(new ArrayList<String>());
-			values.add(new ArrayList<String>());
-			values.add(new ArrayList<String>());
-			values.add(new ArrayList<String>());
+			values.add(new DefaultListModel<String>());
+			values.add(new DefaultListModel<String>());
+			values.add(new DefaultListModel<String>());
+			values.add(new DefaultListModel<String>());
+			
 			while((line=file.readLine())!=null) {
-                for (int i = 0; i < line.length(); i++){
-                    if ((line.charAt(i) == ',' || i + 1 == line.length()) && line.charAt(i-1) != ','){
-                    	String[] splice = line.split(",");
-                    	values.get(0).add(splice[0]);
-                    	values.get(1).add(splice[1]);
-                    	values.get(2).add(splice[2]);
-                    	values.get(3).add(splice[3]);
-                    }
-                }
+            	String[] splice = line.split(",");
+            	values.get(0).addElement(splice[0]);
+            	values.get(1).addElement(splice[1]);
+            	values.get(2).addElement(splice[2]);
+            	values.get(3).addElement(splice[3]);
 			}
 		}catch (Exception e){
 			e.printStackTrace();
